9c31380fad2416ffdd2b1fabe8709d8e4d1e9d9f
adammakesthingsdev <adam@adammakesthings.dev>
2025-01-13 15:07:35 -0500

    Changes header color and adds button for landing page

--- website/src/app/components/header.tsx
+++ website/src/app/components/header.tsx
@@ -4,11 +4,15 @@
 
 import { Navbar, NavbarBrand, NavbarItem } from "@nextui-org/navbar";
 import { ThemeSwitcher } from "../components/themeSwitcher";
+import Link from "next/link";
 
 export default function DocsHeader() {
     return (
-        <Navbar className="bg-content1">
+        <Navbar className="bg-content3">
             <NavbarBrand>Orpheus Pico Docs</NavbarBrand>
+            <NavbarItem>
+                <Link href="/">Homepage</Link>
+            </NavbarItem>
             <NavbarItem>
                 <ThemeSwitcher />
             </NavbarItem>
